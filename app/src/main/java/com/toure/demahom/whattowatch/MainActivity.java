d8d0363a1dded1f0bd56257d8f1f11286c7a1688
DemahomGitHub <demahom.toure@gmail.com>
2015-12-02 15:50:03 +0100

    Projet Android
    Application mobile de critiques de films

---
+++ app/src/main/java/com/toure/demahom/whattowatch/MainActivity.java
@@ -0,0 +1,44 @@
+package com.toure.demahom.whattowatch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.MainThread;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btnLogin = (Button) findViewById(R.id.mainLoginBtnID);
+        btnRegister = (Button) findViewById(R.id.registerBtnID);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Register.class));
+            }
+        });
+    }
+
+    private Button btnLogin;
+    private Button btnRegister;
+}
